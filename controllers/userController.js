103e32a7d679ea87bf288b5354c47e70d380cd3a
asadali57b <asadali57b@gmail.com>
2024-11-12 00:26:33 +0500

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,77 +15,6 @@ const SignupSchema = Joi.object({
     password: Joi.string().min(6).max(1024).required()
 });
 
-// const allUsers = asyncHandler(async (req, res) => {
-//     const keyword = req.query.search
-//       ? {
-        //   $or: [
-        //     { name: { $regex: req.query.search, $options: "i" } },
-        //     { email: { $regex: req.query.search, $options: "i" } },
-        //   ],
-//         }
-//       : {};
-  
-//     const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
-//     res.send(users);
-//   });
-
-//   const registerUser = asyncHandler(async (req, res) => {
-//     const { name, email, password, pic } = req.body;
-  
-//     if (!name || !email || !password) {
-//       res.status(400);
-//       throw new Error("Please Enter all the Feilds");
-//     }
-  
-//     const userExists = await User.findOne({ email });
-  
-//     if (userExists) {
-//       res.status(400);
-//       throw new Error("User already exists");
-//     }
-  
-//     const user = await User.create({
-//       name,
-//       email,
-//       password,
-//       pic,
-//     });
-  
-//     if (user) {
-//       res.status(201).json({
-//         _id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         isAdmin: user.isAdmin,
-//         pic: user.pic,
-//         token: generateToken(user._id),
-//       });
-//     } else {
-//       res.status(400);
-//       throw new Error("User not found");
-//     }
-//   });
-//   const authUser = asyncHandler(async (req, res) => {
-//     const { email, password } = req.body;
-  
-//     const user = await User.findOne({ email });
-  
-//     if (user && (await user.matchPassword(password))) {
-//       res.json({
-//         _id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         isAdmin: user.isAdmin,
-//         pic: user.pic,
-//         token: generateToken(user._id),
-//       });
-//     } else {
-//       res.status(401);
-//       throw new Error("Invalid Email or Password");
-//     }
-//   });
-  
-//   module.exports = { allUsers, registerUser, authUser };
   
 exports.registerUser= async(req,res)=>{
     
@@ -169,12 +98,7 @@ exports.loadDashboard = async (req, res, next) => {
             const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
             res.send(users);
         
-        // const currentUserId = req.user._id; 
-
-        
-        // const users = await User.find({ _id: { $ne: currentUserId } });
-
-        // res.status(200).json({ users });
+       
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
