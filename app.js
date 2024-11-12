a01cb3ec28cbbcaf3fdd35324edf928c26413c98
asadali57b <asadali57b@gmail.com>
2024-11-12 13:39:48 +0500

    new changes

--- app.js
+++ app.js
@@ -1,77 +1,62 @@
+
 const mongoose = require("mongoose");
-const express = require("express");
 const http = require("http");
-require("dotenv").config();
+
+const express = require("express");
+const {Server}=require("socket.io");
 const app = express();
+const server=http.createServer(app);
+const io=new Server(server)
+require("dotenv").config();
+const path = require("path");
 app.use(express.json());
 
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const userRoute=require('./routes/userRoute')
-const chatRoute=require('./routes/chat_routes')
-const messageRoute=require('./routes/message_route')
+const userRoute = require('./routes/userRoute');
+const chatRoute = require('./routes/chat_routes');
+const messageRoute = require('./routes/message_route');
 
-app.use('/',userRoute),
-app.use('/api/chat',chatRoute)
-app.use('/api/message',messageRoute);
-const path = require("path");
+// Routes setup
+app.use('/', userRoute);
+app.use('/api/chat', chatRoute);
+app.use('/api/message', messageRoute);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
-
-mongoose.connect("mongodb://localhost:27017/chat-application", {
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-});
-
-const server=app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-const io=require('socket.io')(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: process.env.socket_origin,
-        // credentials: true,
-    },
-});
-io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-
-    // Listen for chat messages from client
-    socket.on("setup", (userData) => {
-        socket.join(userData._id);
-        console.log(`userId: ${userData._id}`);
-
-        socket.emit("connected");
-        console.log(`userId: ${userData._id}`);
-        // io.emit("message", message); // Broadcast message to all clients
-    });
-    socket.on("Join Chat",(room)=>{
-        socket.join(room);
-        console.log(`User joined room: ${room}`);
-    });
-    socket.on("typing", (room) => {
-        socket.in(room).emit("typing");
-    });
-    socket.on("stop typing", (room) => {
-        socket.in(room).emit("stop typing");
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected from socket.io");
-    });
-    socket.on("new message",(newMessageRecieved)=>{
-        var chat=newMessageRecieved.chat;
-        if(!chat.users) return console.log("chat.users not defined");
-        chat.users.forEach(user=>{
-            if(user._id==newMessageRecieved.sender._id) return;
-            socket.in(user._id).emit("message recieved",newMessageRecieved);
-        })
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/chat-application")
+    .then(() => {
+        console.log("Connected to MongoDB");
     })
-});
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+    io.on("connection", (socket) => {
+        console.log("A new user has connected ",socket.id);
+        socket.on('chatMessage',message => {
+            io.emit('message',message);
+            console.log("A new User Message",message);
+        })
+        socket.on("join_room", (data) => {
+            socket.join(data);
+            console.log(`User with ID: ${socket.id} joined room: ${data}`);
+        });
+
+        socket.on("typing", (room) => {
+            socket.in(room).emit("typing");
+        });
+        socket.on("stop typing", (room) => {
+            socket.in(room).emit("stop typing");
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected from socket.io");
+        });
+        
+    })
